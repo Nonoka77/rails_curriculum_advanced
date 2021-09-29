39e27819532d056b9bcb93751b75c7ab2fd3928a
Nonoka77 <84692432+Nonoka77@users.noreply.github.com>
2021-09-29 09:14:26 +0900

    Add: コメントを追加

--- app/forms/search_articles_form.rb
+++ app/forms/search_articles_form.rb
@@ -9,16 +9,16 @@ class SearchArticlesForm
   attribute :body, :string
 
   def search
-    relation = Article.distinct
+    relation = Article.distinct #重複するArticleを１つにまとめる
 
-    relation = relation.by_category(category_id) if category_id.present?
-    relation = relation.by_author(author_id) if author_id.present?
-    relation = relation.by_tag(tag_id) if tag_id.present?
+    relation = relation.by_category(category_id) if category_id.present? #categoryの検索機能
+    relation = relation.by_author(author_id) if author_id.present?  #authorの検索機能
+    relation = relation.by_tag(tag_id) if tag_id.present?  #tagの検索機能
     title_words.each do |word|
-      relation = relation.title_contain(word)
+      relation = relation.title_contain(word) #タイトルの検索機能
     end
     body_words.each do |word|
-      relation = relation.body_contain(word)
+      relation = relation.body_contain(word) #本文の検索機能
     end
     relation
   end
@@ -26,10 +26,10 @@ class SearchArticlesForm
   private
 
   def title_words
-    title.present? ? title.split(nil) : []
+    title.present? ? title.split(nil) : [] #空白ごとにキーワードを分割する
   end
 
   def body_words
-    body.present? ? body.split('') : []
+    body.present? ? body.split('') : [] #１文字ごとにキーワードを分割する
   end
 end
