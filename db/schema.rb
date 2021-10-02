dbf0eae0d4fbcf8064ffebeeb9eb7625316f1e68
Nonoka77 <84692432+Nonoka77@users.noreply.github.com>
2021-10-02 16:56:34 +0900

    Add: schema.rbにeyecatch_alignを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_02_044513) do
+ActiveRecord::Schema.define(version: 2021_10_02_074023) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2021_10_02_044513) do
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
     t.integer "eyecatch_width"
+    t.integer "eyecatch_align"
     t.index ["author_id"], name: "index_articles_on_author_id"
     t.index ["category_id"], name: "index_articles_on_category_id"
     t.index ["deleted_at"], name: "index_articles_on_deleted_at"
