7d9bfa6e2056be563feee382837e69626e48f8a6
Nonoka77 <84692432+Nonoka77@users.noreply.github.com>
2021-10-06 08:56:10 +0900

    Add: Swiperをインストール

--- app/javascript/articleBlockEditor/index.js
+++ app/javascript/articleBlockEditor/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
 import Editor from './editor'
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+// import styles bundle
+import 'swiper/css/bundle';
+// init Swiper:
+const swiper = new Swiper(...);
 
 (function ($, undefined) {
   $.fn.articleBlockEditor = function () {
